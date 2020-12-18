45b08545eec0344c05a712a3e8a350f8a9121b3d
Melissa <melissa.4.kennedy@gmail.com>
2020-12-18 17:17:23 -0500

    updated dev.env.js

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -3,5 +3,14 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
+      },
+    }
+  }
 })
+
