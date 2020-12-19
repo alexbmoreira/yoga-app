7c5bdd0ac781b0aaf35a686862dca68e5f7d56a8
alexbmoreira <alexbmoreira@gmail.com>
2020-12-19 17:15:58 -0500

    Changed how axios requests are made

--- frontend/src/api/poses.js
+++ frontend/src/api/poses.js
@@ -1,18 +1,8 @@
 import axios from "axios";
 
 async function getPoses() {
-    var records = [];
-
-    // do {
-		var response = await axios.get("/api/poses/");
-
-		records = records.concat(response.data.results);
-    // } while (response.data.next !== null)
-
-	return records
-
-	// const response = await axios.get("/api/poses/");
-	// return response.data.results;
+    const response = await axios.get("/api/poses/");
+	return response.data.results;
 }
 
 export default {
