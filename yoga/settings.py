2c3189c753474c0e4e7b38f4c919cead9ef729cd
alexbmoreira <alexbmoreira@gmail.com>
2021-02-02 21:01:26 -0500

    Python decouple

--- yoga/settings.py
+++ yoga/settings.py
@@ -1,4 +1,5 @@
 import os
+from decouple import config
 import django_heroku
 import dj_database_url
 """
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['namastebuilder.herokuapp.com']
 
