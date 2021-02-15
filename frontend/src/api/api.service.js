9e0a2ed06336ad3811ff33e7675ae3ca409109b5
alexbmoreira <alexbmoreira@gmail.com>
2021-02-15 14:39:44 -0500

    Removed 5s timeout

--- frontend/src/api/api.service.js
+++ frontend/src/api/api.service.js
@@ -3,7 +3,6 @@ import Cookies from 'js-cookie'
 
 export default axios.create({
   baseURL: process.env.VUE_APP_URL + '/api',
-  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
     'X-CSRFTOKEN': Cookies.get('csrftoken')
